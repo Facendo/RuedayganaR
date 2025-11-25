b2ecd8bc0fa08e25970c9c5d678c8898aded8d0b
Francisco Hernandez <hernandezfrancisco672@gmail.com>
2025-11-24 21:08:47 -0400

    First commit

--- routes/cliente.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use App\Http\Controllers\ClienteController;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/compra/{id_sorteo}',[ClienteController::class, 'index'])->name('compra');
-
-Route::post('/compra',[ClienteController::class, 'store'])->name('cliente.store');
--- routes/premio.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use App\Http\Controllers\PremioController;
-use Illuminate\Support\Facades\Route;
-
-Route::post('/admin',[PremioController::class, 'store'])->name('premio.store')->middleware('auth');
